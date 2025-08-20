872872cb6783ffe64bca69a72a769520dba1258f
Matthew Sells <matthew.sells@bonus.ly>
2025-08-20 09:46:29 -0500

    Set up dependencies for authentication access and testing

--- foody_api/config/initializers/cors.rb
+++ foody_api/config/initializers/cors.rb
@@ -1,16 +1,12 @@
-# Be sure to restart your server when you modify this file.
+# CORS configuration for API access from frontend
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Allow requests from the React frontend (running on port 5173)
+    origins "http://localhost:5173"
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true  # Allow cookies to be sent with requests
+  end
+end
